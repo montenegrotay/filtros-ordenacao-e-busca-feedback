56d86a03c875cb985ac7aecb610e38225447ed7a
Taynara <montenegro.taynara@gmail.com>
2022-10-27 15:06:02 -0300

    Exercicios Filtros, Ordenação e Busca

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pokemons from "./pokemon/pokemon.json";
 import PokemonCard from "./components/PokemonCard/PokemonCard";
 import { getColors } from "./utils/ReturnCardColor";
 import Header from "./components/Header/Header.js";
+
 const GlobalStyle = createGlobalStyle`
   *{
     padding: 0;
@@ -13,14 +14,18 @@ const GlobalStyle = createGlobalStyle`
   
   }
 `;
+
 const CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
   justify-items: center;
 `;
+
 function App() {
   const [pesquisa, setPesquisa] = useState("");
   const [idFilter, setIdFilter] = useState("");
+  const [tipos, setTipos] = useState("")
+  const [ordemAlfabetica, setOrdemAlfabetica] = useState("")
 
   return (
     <>
@@ -30,14 +35,43 @@ function App() {
         setIdFilter={setIdFilter}
         pesquisa={pesquisa}
         setPesquisa={setPesquisa}
+        tipos={tipos}
+        setTipos={setTipos}
+        ordemAlfabetica={ordemAlfabetica}
+        setOrdemAlfabetica={setOrdemAlfabetica}
       />
+
       <CardsContainer>
         {pokemons.filter((pokemon) => {
           return idFilter ? pokemon.id.includes(idFilter) : pokemon
         })
+
           .filter((pokemon) => {
             return pokemon.name.english.toLowerCase().includes(pesquisa.toLowerCase());
           })
+
+          //filtrar por tipo
+          .filter((pokemon) => {
+            return tipos ? pokemon.type.includes(tipos) : pokemon
+          })
+
+          //filtrar por ordem
+          .sort((a, b)=>{
+            if(ordemAlfabetica === "crescente"){
+              if(a.name.english < b.name.english) {
+                return -1
+              } else {
+                return 1
+              }
+            } else if (ordemAlfabetica === "decrescente"){
+              if(a.name.english < b.name.english) {
+                return 1
+              } else {
+                return -1
+              }
+            }
+          })
+
           .map((pokemon) => {
             return (
               <PokemonCard
